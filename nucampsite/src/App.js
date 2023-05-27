574c142a7599ce0f35ba19810974d92e8b39a481
jason.schaeffer <schaeffer.jason@gmail.com>
2023-05-27 12:41:49 -0400

    Week 1 Workshop Assignment

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -1,21 +1,14 @@
-import React from 'react';
-import CampsiteCard from './features/campsites/CampsiteCard.js';
-import { CAMPSITES } from './app/shared/CAMPSITES';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+import Header from './components/Header';
+import CampsitesList from './features/campsites/CampsitesList';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
     return (
         <div className='App'>
-            <Navbar dark color='primary' sticky='top' expand='md'>
-                <Container>
-                    <NavbarBrand href='/'>
-                        <img src={NucampLogo} alt='nucamp logo' />
-                    </NavbarBrand>
-                </Container>
-            </Navbar>
-            <CampsiteCard campsite={CAMPSITES[0]} />
+            <Header />
+            <CampsitesList />
+            <Footer />
         </div>
     );
 }
--- nucampsite/src/features/campsites/CampsitesList.js
+++ nucampsite/src/features/campsites/CampsitesList.js
@@ -4,15 +4,16 @@ import CampsiteCard from "./CampsiteCard";
 
 const CampsitesList = () => {
     return (
-        <Card>
-            <CardImg 
-                width='100%'
-                src={props.campsite.image}
-                alt={props.campsite.name}
-            />
-            <CardImgOverlay> 
-                <CardTitle>{props.campsite.name}</CardTitle>
-            </CardImgOverlay>
-        </Card>
-    );
+        <Row className='ms-auto'>
+            {CAMPSITES.map((campsite) => {
+                return (
+                    <Col md='5' className='m-4' key={campsite.id}>
+                        <CampsiteCard campsite={campsite} />
+                    </Col>
+                );
+            })}
+        </Row>
+    )
 }
+
+export default CampsitesList;
