c47f804213d2c6d0ecfc085db83dc11b6d00fc1f
jason.schaeffer <schaeffer.jason@gmail.com>
2023-06-02 21:32:39 -0400

    CampsiteDetailPage with useParams

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -1,13 +1,25 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
-import CampsitesList from "./features/campsites/CampsitesList";
 import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import "./App.css";
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <CampsitesList />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="contact" element={<ContactPage />} />
+                <Route path="directory" element={<CampsitesDirectoryPage />} />
+                <Route
+                    path="directory/:campsiteId"
+                    element={<CampsiteDetailPage />}
+                />
+            </Routes>
             <Footer />
         </div>
     );
